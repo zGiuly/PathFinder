bcad9391404ae510747ee60cfa1de1da206ad77c
Giulia <giuliapvpgirls@gmail.com>
2020-04-20 14:00:01 +0200

    20/04/2020

--- src/it/artmistech/pathfinder/listeners/WarpSignListener.java
+++ src/it/artmistech/pathfinder/listeners/WarpSignListener.java
@@ -1,9 +1,12 @@
 package it.artmistech.pathfinder.listeners;
 
 import it.artmistech.pathfinder.PathFinder;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.List;
 
 public class WarpSignListener extends AbstractListener {
     public WarpSignListener(PathFinder pathFinder) {
@@ -15,8 +18,18 @@ public class WarpSignListener extends AbstractListener {
         if(!event.getPlayer().hasPermission("pathfinder.sign.warp.create")) return;
 
         if(event.getLine(0).equalsIgnoreCase("[Warp]") && event.getLines().length == 2) {
-            
+            List<String> format = configStringList("warp-sign-format");
+
+            for (int i = 0; i < format.size(); i++) {
+                event.setLine(1, parseFormat(format.get(i)));
+            }
         }
+    }
+
+    public String parseFormat(String data) {
+        String parsed = data.replaceAll("%warpname%", "");
+
+        return ChatColor.translateAlternateColorCodes('&', parsed);
     }
 
     @EventHandler
